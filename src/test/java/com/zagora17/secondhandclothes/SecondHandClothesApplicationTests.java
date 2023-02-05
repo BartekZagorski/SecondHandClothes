8da936446cc1351e879f4d2ca9b4d34d0424cd29
zagora17 <zagora171991@gmail.com>
2023-02-05 12:24:03 +0100

    Add Spring Boot starter

---
+++ src/test/java/com/zagora17/secondhandclothes/SecondHandClothesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zagora17.secondhandclothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandClothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
