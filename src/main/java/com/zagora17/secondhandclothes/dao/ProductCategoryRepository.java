e7a483f3ac630f0145d5991c5b78740f8fe4ecd9
zagora17 <zagora171991@gmail.com>
2023-02-24 13:26:26 +0100

    add cross origin annotation to the repos

--- src/main/java/com/zagora17/secondhandclothes/dao/ProductCategoryRepository.java
+++ src/main/java/com/zagora17/secondhandclothes/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.zagora17.secondhandclothes.dao;
 import com.zagora17.secondhandclothes.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "productCategories", path = "product-categories")
+@CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/com/zagora17/secondhandclothes/dao/ProductRepository.java
+++ src/main/java/com/zagora17/secondhandclothes/dao/ProductRepository.java
@@ -3,7 +3,9 @@ package com.zagora17.secondhandclothes.dao;
 import com.zagora17.secondhandclothes.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "products", path = "products")
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
