abfb3976079f4c3d41ea5c3d9d018bf8805c41ea
zagora17 <zagora171991@gmail.com>
2023-02-25 06:58:48 +0100

    add new query method inside ProductRepository to find products by its category

--- src/main/java/com/zagora17/secondhandclothes/dao/ProductRepository.java
+++ src/main/java/com/zagora17/secondhandclothes/dao/ProductRepository.java
@@ -1,11 +1,16 @@
 package com.zagora17.secondhandclothes.dao;
 
 import com.zagora17.secondhandclothes.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "products", path = "products")
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
