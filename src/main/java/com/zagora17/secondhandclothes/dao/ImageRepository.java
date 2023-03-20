2d4490cd2ff966bc084a31168e5e4d87a912ceee
zagora17 <zagora171991@gmail.com>
2023-03-20 05:39:51 +0100

    add ImageRepository

---
+++ src/main/java/com/zagora17/secondhandclothes/dao/ImageRepository.java
@@ -0,0 +1,12 @@
+package com.zagora17.secondhandclothes.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.awt.*;
+
+@RepositoryRestResource(collectionResourceRel = "images", path = "images")
+@CrossOrigin("http://localhost:4200")
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
