460eaa2232da870f6112af05df54083b6774eb90
zagora17 <zagora171991@gmail.com>
2023-04-03 05:36:48 +0200

    Add class Place

---
+++ src/main/java/com/zagora17/secondhandclothes/entity/Place.java
@@ -0,0 +1,22 @@
+package com.zagora17.secondhandclothes.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "places")
+@Getter
+@Setter
+public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+
+}
