3f69229d8e0fb11102a7667e5e75a8bc6d63fbdf
zagora17 <zagora171991@gmail.com>
2023-04-17 05:54:24 +0200

    update CustomerRepository, add method and annotation

--- src/main/java/com/zagora17/secondhandclothes/dao/CustomerRepository.java
+++ src/main/java/com/zagora17/secondhandclothes/dao/CustomerRepository.java
@@ -2,8 +2,13 @@ package com.zagora17.secondhandclothes.dao;
 
 import com.zagora17.secondhandclothes.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "customers", path = "customers")
+@CrossOrigin("http://localhost:4200")
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Customer findByEmail(String email);
 }
