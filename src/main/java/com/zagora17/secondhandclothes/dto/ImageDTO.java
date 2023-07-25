bfacb4ec5d6a11949fe252662079114e550e066b
zagora17 <zagora171991@gmail.com>
2023-07-25 22:46:51 +0200

    introduce ImageDTO
    create class imageDTO to be able to pass image id besides the url

---
+++ src/main/java/com/zagora17/secondhandclothes/dto/ImageDTO.java
@@ -0,0 +1,13 @@
+package com.zagora17.secondhandclothes.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ImageDTO {
+
+    Long id;
+    String url;
+
+}
