d6d6373271295e95dbeebc305a67256a3533c926
zagora17 <zagora171991@gmail.com>
2023-12-07 17:07:31 +0100

    Add resource handler and springbootServletInitializer Introduce those two features in order to properly run application on the production using tomcat externall server

--- src/main/java/com/zagora17/secondhandclothes/SecondHandClothesApplication.java
+++ src/main/java/com/zagora17/secondhandclothes/SecondHandClothesApplication.java
@@ -2,9 +2,10 @@ package com.zagora17.secondhandclothes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SecondHandClothesApplication {
+public class SecondHandClothesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecondHandClothesApplication.class, args);
--- src/main/java/com/zagora17/secondhandclothes/config/MyAppConfig.java
+++ src/main/java/com/zagora17/secondhandclothes/config/MyAppConfig.java
@@ -2,8 +2,15 @@ package com.zagora17.secondhandclothes.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
+
 @Configuration
 class MyAppConfig implements WebMvcConfigurer {
     @Value("${allowed.origins}")
@@ -17,4 +24,22 @@ class MyAppConfig implements WebMvcConfigurer {
 
         cors.addMapping(basePath + "/**").allowedOrigins(theAllowedOrigins);
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/public/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath,
+                                                   Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/public/index.html");
+                    }
+                });
+    }
+
 }
